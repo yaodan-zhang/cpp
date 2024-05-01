b6d8ab3645b16c765fb904ca7dc4eacb4669429a
KatherineZh000 <yaodanzhang00@gmail.com>
2024-05-01 09:03:10 -0600

    added tests

--- m10/type_tuple.cpp
+++ m10/type_tuple.cpp
@@ -1,13 +1,10 @@
-/*
-    Functional programming in C++ for type modifications.
-*/
+/* Functional programming type conversion */
 #include<tuple>
 #include<iostream>
+#include <type_traits>
 using namespace std;
 
-/* 
-    Concatenate the types in two tuples
-*/
+/* Appending the types in two tuples */
 template<class First, class Second> struct Append;
 template<typename... Ts, typename... Us>
 struct Append<tuple<Ts...>, tuple<Us...> >
@@ -16,16 +13,14 @@ using type = tuple<Ts..., Us...>;
 };
 
 /*  
-    A ReplaceAll<TL, T, U> template that replaces all occurrence of type T in TL with type U.
-    For example, ReplaceAll<tuple<char, int, char>,char, double>::type is tuple<double, int, double>.
+    A ReplaceAll<TL, T, U> template that replaces all occurrence of T in TL with U.
+    For example, ReplaceAll<tuple<char, int, char>,char, double>::type 
+    should be tuple<double, int, double>
 */
-template<typename T, typename A, typename B> struct ReplaceAll; // primary template - calling format
-template<typename T, typename A, typename B>
-using ReplaceAll_t = typename ReplaceAll<T,A,B>::type;
-
+template<typename T, typename A, typename B> struct ReplaceAll;
 template<typename A, typename B>
 struct ReplaceAll<tuple<>, A, B> {
-    using type = tuple<>;
+using type = tuple<>;
 };
 template<typename... Ts, typename A, typename B>
 struct ReplaceAll<tuple<A, Ts...>, A, B>
@@ -36,14 +31,13 @@ struct ReplaceAll<tuple<H, Ts...>, A, B>
 : public Append<tuple<H>, typename ReplaceAll<tuple<Ts...>, A, B>::type> {
 };
 
+static_assert(is_same_v<ReplaceAll<tuple<char, int, char>,char,double>::type, tuple<double, int, double>>);
+
 /* 
-    A Reverse template that reverses the types in a tuple.
-    For example, Reverse<tuple<A, B>>::type is tuple<B, A>.
+    A Reverse template that reverses the parameters of a tuple
+    For example, Reverse<tuple<A, B, C, B>, A, B>::type will be tuple<B, A, C, A>
 */
 template<typename T, typename A, typename B> struct Reverse;
-template<typename T>
-using Reverse_t = Reverse<t>::type;
-
 template<typename A, typename B>
 struct Reverse<tuple<>, A, B> {
 using type = tuple<>;
@@ -58,7 +52,11 @@ template<typename... Ts, typename A, typename B>
 struct Reverse<tuple<B, Ts...>, A, B>
 : public Append<tuple<A>, typename Reverse<tuple<Ts...>, A, B>::type> {
 };
+// Skip
 template<typename H, typename... Ts, typename A, typename B>
 struct Reverse<tuple<H, Ts...>, A, B>
 : public Append<tuple<H>, typename Reverse<tuple<Ts...>, A, B>::type> {
 };
+
+static_assert(is_same_v<Reverse<tuple<char, int>, char, int>::type, tuple<int, char>>);
+static_assert(is_same_v<Reverse<tuple<char, int, double, int>, int, char>::type, tuple<int, char, double, char>>);
